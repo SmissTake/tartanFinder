324595af94e24ccd70a24a544b142cdc356ec33f
Gwion Tangi <lil.smiss7@gmail.com>
2023-09-08 13:56:09 +0200

    updated UParse to find html tag attribute

--- src/utils/UParse.ts
+++ src/utils/UParse.ts
@@ -6,8 +6,17 @@ export default async function UParse(html: string, targets: IResults) {
   const results: {[key: string]: string | undefined} = {};
   for (const key in targets) {
     if (targets.hasOwnProperty(key)) {
-      const text = dom.window.document.querySelector(targets[key])?.textContent;
-      results[key] = text ?? undefined;
+      const selector = targets[key];
+      const element = dom.window.document.querySelector(selector);
+      if (element) {
+        if (selector === 'canvas') {
+          const tabulation = element.getAttribute('tabulation');
+          results[key] = tabulation ?? undefined;
+        } else {
+          const text = element.textContent;
+          results[key] = text ?? undefined;
+        }
+      }
     }
   }
   return results;
